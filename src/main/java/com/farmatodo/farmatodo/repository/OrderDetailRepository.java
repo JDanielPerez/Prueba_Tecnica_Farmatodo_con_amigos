a2c9ca3f424b01b11005fb0e6d552eb54e07ad59
JDanielPerez <juancho.nac13@gmail.com>
2025-09-05 08:56:27 -0500

    segundo push

--- src/main/java/com/farmatodo/farmatodo/repository/OrderDetailRepository.java
+++ src/main/java/com/farmatodo/farmatodo/repository/OrderDetailRepository.java
@@ -4,6 +4,11 @@ import com.farmatodo.farmatodo.model.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+
+    List<OrderDetail> findByOrderId(Long orderId);
+
 }
--- src/main/java/com/farmatodo/farmatodo/service/CustomerService.java
+++ src/main/java/com/farmatodo/farmatodo/service/CustomerService.java
@@ -19,17 +19,16 @@ public class CustomerService {
 
     /*
      * Valida que los datos de contacto del cliente sean únicos antes de su registro.
-     * <p>
      * Este método verifica que el correo electrónico y el número de teléfono del cliente
      * no estén previamente registrados en la base de datos. Si alguno ya existe, lanza
      * una excepción indicando el conflicto.
-     * </p>
      *
      * @param customer Objeto {@link Customer} que contiene los datos a validar.
      * @throws IllegalArgumentException Si el correo electrónico o el número de teléfono ya están registrados.
      */
     private void validateCustomer(Customer customer){
 
+        //adicional deberia tener otra condición que valide si el formato del correo electronico es valido?
         if(customerRepository.existByMail(customer.getEmail())){
             throw new IllegalArgumentException("Este correo electronico ya se encuentra registrado.");
         }
